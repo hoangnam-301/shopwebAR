1dc1ba65d2d7443d55694fc2fb3cd90e0efc254d
hoangnam-301 <email-ng-k-github@v-d.com>
2025-12-24 07:30:07 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    $path = '/var/www/frontend/User_interface/index.html'; 
+    
+    if (file_exists($path)) {
+        return file_get_contents($path);
+    }
+    
+    return response("Không tìm thấy file Frontend tại: " . $path, 404);
+})->where('any', '.*');
+
